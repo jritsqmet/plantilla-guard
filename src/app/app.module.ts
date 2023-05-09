637e4e98605a7cae3413a22df493adba3ecfb7d0
jroserog <jroserog@itsqmet.edu.ec>
2023-05-08 21:20:30 -0500

    plantilla guard-login realizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PrivadoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
